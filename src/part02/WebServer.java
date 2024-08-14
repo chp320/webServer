964dc48ed062a61ab2c16a227b01242a5f402e4e
chp320 <chp320@naver.com>
2024-08-15 01:25:34 +0900

    POST 메소드 입력 폼 예시와 client가 요청한 내용을 server_recv.txt 파일로 기록하는 WebServer 기능 구현

---
+++ src/part02/WebServer.java
@@ -0,0 +1,29 @@
+package part02;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(8001);
+             FileOutputStream fos = new FileOutputStream("server_recv.txt")) {
+            System.out.println("클라이언트 접속을 기다리고 있습니다.");
+            Socket socket = server.accept();
+            System.out.println("클라이언트 접속");
+
+            int ch;
+            // 클라이언트에서 송신한 내용을 server_recv.txt에 출력
+            InputStream input = socket.getInputStream();
+            while ((ch = input.read()) != -1) {
+                fos.write(ch);
+            }
+            socket.close();
+            System.out.println("통신이 종료되었습니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
